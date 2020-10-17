8dad7ba14bf6dc3666f5a3eef9c5aa7ec550f32d
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 23:33:29 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan13-Relational/src/pboif2/pkg10119048/latihan13/relational/PBOIF210119048Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Relational
+ */
+public class PBOIF210119048Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
